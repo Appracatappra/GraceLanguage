72f825bba0233f89a503f33274b7275aaff5df48
Kevin Mullins <kevin_mullins@appracatappra.com>
2024-01-18 15:06:29 -0600

    Negatives
    Handle negative numbers with a cheat for now.

--- Sources/GraceLanguage/Compiler/GraceCompiler.swift
+++ Sources/GraceLanguage/Compiler/GraceCompiler.swift
@@ -951,6 +951,28 @@ open class GraceCompiler {
             return nil
         }
         
+        // Add negateInt.
+        executable.register(name: "negateInt", parameterNames: ["value"], parameterTypes: [.any], returnType: .int) { parameters in
+            var result:Int = 0
+            
+            if let value = parameters["value"] {
+                result = 0 - value.int
+            }
+            
+            return GraceVariable(name: "result", value: "\(result)", type: .int)
+        }
+        
+        // Add negateInt.
+        executable.register(name: "negateFloat", parameterNames: ["value"], parameterTypes: [.any], returnType: .float) { parameters in
+            var result:Float = 0
+            
+            if let value = parameters["value"] {
+                result = 0 - value.float
+            }
+            
+            return GraceVariable(name: "result", value: "\(result)", type: .float)
+        }
+        
         // Add array item count.
         executable.register(name: "count", parameterNames: ["array"], parameterTypes: [.any], returnType: .int) { parameters in
             var length:Int = 0
--- Tests/GraceLanguageTests/GraceLanguageTests.swift
+++ Tests/GraceLanguageTests/GraceLanguageTests.swift
@@ -24,4 +24,52 @@ final class GraceLanguageTests: XCTestCase {
         let result = try GraceRuntime.shared.expandMacros(in: "The answer is: @intMath(40,'+',2)")
         XCTAssert(result == "The answer is: 42")
     }
+    
+    func testNegativeInt() throws {
+        let code = """
+        import StandardLib;
+        
+        main {
+            var n:int = 5;
+        
+            return @negateInt($n);
+        }
+        """
+        
+        let result = try GraceRuntime.shared.run(program: code)
+        
+        XCTAssert(result?.int == -5)
+    }
+    
+    func testNegativeFloat() throws {
+        let code = """
+        import StandardLib;
+        
+        main {
+            var n:float = 5.0;
+        
+            return @negateFloat($n);
+        }
+        """
+        
+        let result = try GraceRuntime.shared.run(program: code)
+        
+        XCTAssert(result?.float == -5.0)
+    }
+    
+    func testNegativeCheat() throws {
+        let code = """
+        import StandardLib;
+        
+        main {
+            var n:int = 5;
+        
+            return ($n + '-1');
+        }
+        """
+        
+        let result = try GraceRuntime.shared.run(program: code)
+        
+        XCTAssert(result?.float == 4)
+    }
 }
