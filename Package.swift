1031c76395cad15bd5fa423ef14fc7aa075b3953
Kevin Mullins <kevin_mullins@appracatappra.com>
2025-05-13 10:18:45 -0500

    Update to v6.1
    Updated to Swift v6.1 compiler.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,7 +25,10 @@ let package = Package(
         .target(
             name: "GraceLanguage",
             dependencies: ["LogManager", "SwiftletUtilities", "SimpleSerializer"],
-            resources: [.process("Resources")]
+            resources: [.process("Resources")],
+            swiftSettings: [
+                .swiftLanguageMode(.v6)
+            ]
         ),
         .testTarget(
             name: "GraceLanguageTests",
